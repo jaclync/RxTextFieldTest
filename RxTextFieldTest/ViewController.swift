ad4d13689d3e75aef7d1dd290598154ab31ab36f
Jaclyn Chen <watertranquil@gmail.com>
2017-03-14 23:17:36 -0700

    Add example code.

--- RxTextFieldTest/ViewController.swift
+++ RxTextFieldTest/ViewController.swift
@@ -6,20 +6,39 @@
 //  Copyright © 2017 JC. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class ViewController: UIViewController {
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = UIColor.orange
+
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor.white
+
+        view.addSubview(textField)
+
+        // Add autolayout constraints.
+        let margins = view.layoutMarginsGuide
+        textField.centerXAnchor.constraint(equalTo: margins.centerXAnchor).isActive = true
+        textField.centerYAnchor.constraint(equalTo: margins.centerYAnchor).isActive = true
+        textField.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 150).isActive = true
+
+        textField.rx.text.orEmpty
+            .subscribe(onNext: { text in
+                print(text)
+                if text == "たべます" {
+                    textField.textColor = UIColor.green
+                    textField.isUserInteractionEnabled = false
+                }
+            })
+            .addDisposableTo(disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
